92178660d72d825e787e8f0970ae13aaad7786d7
Miguel <mjl38@users.noreply.github.com>
2021-10-28 06:22:48 +0100

    Add project files.

---
+++ QRCodes/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using QREncodeDecodeLibrary;
+
+namespace QRCodes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            //EncodeDecode encodeDecode = new EncodeDecode();
+            string result = QREncodeDecode.EncodeTextToQRImage("hello my name is migueliiiito", "c:/temp2/hello.png");
+            result = QREncodeDecode.DecodeQRImageToText("c:/temp2/hello.png");
+            result = "";
+        }
+    }
+}
---
+++ QREncodeDecode/EncodeDecode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+
+namespace QREncodeDecodeLibrary
+{
+    public class QREncodeDecode
+    {
+        public static string EncodeTextToQRImage(string text, string imageFilename)
+        {
+            BarcodeWriter writer = new BarcodeWriter();
+
+            writer.Format = BarcodeFormat.QR_CODE;
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions()
+            {
+                Width = 200,
+                Height = 200
+            };
+            writer.Options = options;
+
+            try
+            {
+                Bitmap result = writer.Write(text);
+                result.Save(imageFilename);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "SUCCESS"; 
+        }
+
+        public static string DecodeQRImageToText(string imageFilename)
+        {
+            Image imageFile = Image.FromFile(imageFilename) as Bitmap;
+            BarcodeReader reader = new BarcodeReader();
+
+            string result = "";
+            
+            try
+            {
+                result = reader.Decode((Bitmap)imageFile).ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return result;
+        }
+    }
+}
